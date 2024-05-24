dc67fcaf77653e8b8bef21bc7f1c7d3bc4daeddf
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2024-05-24 07:16:06 +0300

    aliases

--- src/aliases.rs
+++ src/aliases.rs
@@ -2,59 +2,68 @@
 
 use crate::{Duration, Fraction, Instant, Rate};
 
+const NANO: Fraction = Fraction::NANO;
+const MICRO: Fraction = Fraction::MICRO;
+const MILLI: Fraction = Fraction::MILLI;
+const ONE: Fraction = Fraction::ONE;
+const MIN: Fraction = Fraction::new(60, 1);
+const HOUR: Fraction = Fraction::new(3600, 1);
+const KILO: Fraction = Fraction::KILO;
+const MEGA: Fraction = Fraction::MEGA;
+
 /// Alias for nanosecond duration
-pub type NanosDuration<T> = Duration<T, { Fraction::NANO }>;
+pub type NanosDuration<T> = Duration<T, NANO>;
 
 /// Alias for nanosecond duration (`u32` backing storage)
-pub type NanosDurationU32 = Duration<u32, { Fraction::MICRO }>;
+pub type NanosDurationU32 = Duration<u32, MICRO>;
 
 /// Alias for nanosecond duration (`u64` backing storage)
-pub type NanosDurationU64 = Duration<u64, { Fraction::NANO }>;
+pub type NanosDurationU64 = Duration<u64, NANO>;
 
 /// Alias for microsecond duration
-pub type MicrosDuration<T> = Duration<T, { Fraction::MICRO }>;
+pub type MicrosDuration<T> = Duration<T, MICRO>;
 
 /// Alias for microsecond duration (`u32` backing storage)
-pub type MicrosDurationU32 = Duration<u32, { Fraction::MICRO }>;
+pub type MicrosDurationU32 = Duration<u32, MICRO>;
 
 /// Alias for microsecond duration (`u64` backing storage)
-pub type MicrosDurationU64 = Duration<u64, { Fraction::MICRO }>;
+pub type MicrosDurationU64 = Duration<u64, MICRO>;
 
 /// Alias for millisecond duration
-pub type MillisDuration<T> = Duration<T, { Fraction::MILLI }>;
+pub type MillisDuration<T> = Duration<T, MILLI>;
 
 /// Alias for millisecond duration (`u32` backing storage)
-pub type MillisDurationU32 = Duration<u32, { Fraction::MILLI }>;
+pub type MillisDurationU32 = Duration<u32, MILLI>;
 
 /// Alias for millisecond duration (`u64` backing storage)
-pub type MillisDurationU64 = Duration<u64, { Fraction::MILLI }>;
+pub type MillisDurationU64 = Duration<u64, MILLI>;
 
 /// Alias for second duration
-pub type SecsDuration<T> = Duration<T, { Fraction::ONE }>;
+pub type SecsDuration<T> = Duration<T, ONE>;
 
 /// Alias for second duration (`u32` backing storage)
-pub type SecsDurationU32 = Duration<u32, { Fraction::ONE }>;
+pub type SecsDurationU32 = Duration<u32, ONE>;
 
 /// Alias for second duration (`u64` backing storage)
-pub type SecsDurationU64 = Duration<u64, { Fraction::ONE }>;
+pub type SecsDurationU64 = Duration<u64, ONE>;
 
 /// Alias for minutes duration
-pub type MinutesDuration<T> = Duration<T, { Fraction::new(60, 1) }>;
+pub type MinutesDuration<T> = Duration<T, MIN>;
 
 /// Alias for minutes duration (`u32` backing storage)
-pub type MinutesDurationU32 = Duration<u32, { Fraction::new(60, 1) }>;
+pub type MinutesDurationU32 = Duration<u32, MIN>;
 
 /// Alias for minutes duration (`u64` backing storage)
-pub type MinutesDurationU64 = Duration<u64, { Fraction::new(60, 1) }>;
+pub type MinutesDurationU64 = Duration<u64, MIN>;
 
 /// Alias for hours duration
-pub type HoursDuration<T> = Duration<T, { Fraction::new(3_600, 1) }>;
+pub type HoursDuration<T> = Duration<T, HOUR>;
 
 /// Alias for hours duration (`u32` backing storage)
-pub type HoursDurationU32 = Duration<u32, { Fraction::new(3_600, 1) }>;
+pub type HoursDurationU32 = Duration<u32, HOUR>;
 
 /// Alias for hours duration (`u64` backing storage)
-pub type HoursDurationU64 = Duration<u64, { Fraction::new(3_600, 1) }>;
+pub type HoursDurationU64 = Duration<u64, HOUR>;
 
 /// Alias for durations that come from timers with a specific frequency
 pub type TimerDuration<T, const FREQ_HZ: u32> = Duration<T, { Fraction::new(1, FREQ_HZ) }>;
@@ -79,31 +88,31 @@ pub type TimerInstantU64<const FREQ_HZ: u32> = Instant<u64, { Fraction::new(1, F
 // -------------------------------
 
 /// Alias for hertz rate
-pub type Hertz<T> = Rate<T, { Fraction::ONE }>;
+pub type Hertz<T> = Rate<T, ONE>;
 
 /// Alias for hertz rate (`u32` backing storage)
-pub type HertzU32 = Rate<u32, { Fraction::ONE }>;
+pub type HertzU32 = Rate<u32, ONE>;
 
 /// Alias for hertz rate (`u64` backing storage)
-pub type HertzU64 = Rate<u64, { Fraction::ONE }>;
+pub type HertzU64 = Rate<u64, ONE>;
 
 /// Alias for kilohertz rate
-pub type Kilohertz<T> = Rate<T, { Fraction::KILO }>;
+pub type Kilohertz<T> = Rate<T, KILO>;
 
 /// Alias for kilohertz rate (`u32` backing storage)
-pub type KilohertzU32 = Rate<u32, { Fraction::KILO }>;
+pub type KilohertzU32 = Rate<u32, KILO>;
 
 /// Alias for kilohertz rate (`u64` backing storage)
-pub type KilohertzU64 = Rate<u64, { Fraction::KILO }>;
+pub type KilohertzU64 = Rate<u64, KILO>;
 
 /// Alias for megahertz rate
-pub type Megahertz<T> = Rate<T, { Fraction::MEGA }>;
+pub type Megahertz<T> = Rate<T, MEGA>;
 
 /// Alias for megahertz rate (`u32` backing storage)
-pub type MegahertzU32 = Rate<u32, { Fraction::MEGA }>;
+pub type MegahertzU32 = Rate<u32, MEGA>;
 
 /// Alias for megahertz rate (`u64` backing storage)
-pub type MegahertzU64 = Rate<u64, { Fraction::MEGA }>;
+pub type MegahertzU64 = Rate<u64, MEGA>;
 
 /// Alias for rate that come from timers with a specific frequency
 pub type TimerRate<T, const FREQ_HZ: u32> = Rate<T, { Fraction::new(FREQ_HZ, 1) }>;
